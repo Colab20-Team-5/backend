0476c6831f457b53e61f0713b090b4e39b593dd6
Piyush Dhall <contact@piyush.ovh>
2023-07-01 08:20:00 +0530

    backend init and mongoose boilerplate

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = 3000;
+
+// Mongoose Wrapper
+const MongooseWrapper = {
+  connect: (mongoURI) => {
+    mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+
+    mongoose.connection.on('connected', () => {
+      console.log('Connected to MongoDB');
+    });
+
+    mongoose.connection.on('error', (err) => {
+      console.error('Error connecting to MongoDB:', err);
+    });
+
+    mongoose.connection.on('disconnected', () => {
+      console.log('Disconnected from MongoDB');
+    });
+  },
+
+  disconnect: () => {
+    mongoose.disconnect();
+  },
+
+  getMongooseInstance: () => {
+    return mongoose;
+  },
+};
+
+// Replace 'your_mongo_uri' with your actual MongoDB connection URI
+const mongoURI = 'mongodb://localhost:27017/your_database_name';
+
+// Connect to MongoDB using the Mongoose wrapper
+MongooseWrapper.connect(mongoURI);
+
+// Middleware for accessing the Mongoose instance
+app.use((req, res, next) => {
+  req.mongoose = MongooseWrapper.getMongooseInstance();
+  next();
+});
+
+// Routes and other middleware can be added here
+app.get('/', (req, res) => {
+  res.send('Hello from Express with Mongoose!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+// Gracefully disconnect from MongoDB when the server is closed
+process.on('SIGINT', () => {
+  MongooseWrapper.disconnect();
+  process.exit(0);
+});
