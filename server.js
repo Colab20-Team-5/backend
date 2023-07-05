301fcdec12140b84acb3083af9be9f23102c2e73
Piyush Dhall <contact@piyush.ovh>
2023-07-05 10:20:00 +0530

    sample apis and userModel

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const User = require('./userModel'); // Import the User model from userModel.js
 
 const app = express();
 const PORT = 3000;
@@ -48,10 +50,41 @@ app.use((req, res, next) => {
   next();
 });
 
+// Parse incoming requests with JSON payloads
+app.use(bodyParser.json());
+
 // Routes and other middleware can be added here
 app.get('/', (req, res) => {
-  res.send('Hello from Express with Mongoose!');
+  res.send('Hello from BeFit!');
 });
+
+// Sample API: Fetch all users
+app.get('/api/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch users' });
+  }
+});
+
+// Sample API: Create a new user
+app.post('/api/users', async (req, res) => {
+  const { name, email } = req.body;
+
+  if (!name || !email) {
+    return res.status(400).json({ error: 'Name and email are required fields' });
+  }
+
+  try {
+    const newUser = new User({ name, email });
+    await newUser.save();
+    res.json(newUser);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to create a new user' });
+  }
+});
+
 
 // Start the server
 app.listen(PORT, () => {
